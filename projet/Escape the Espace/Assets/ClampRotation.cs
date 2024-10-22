3a07af372d836b117cff2edf80630e9b2ca02c2d
Michael Roussel <michaelroussel404@outlook.com>
2024-10-22 18:32:45 -0400

    Création d'un gym pour le joystick et ses features en plus d'ajouts de dépendances utiles

---
+++ projet/Escape the Espace/Assets/ClampRotation.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+
+public class ClampRotation : MonoBehaviour
+{
+    [SerializeField] private Vector3 minRotation;
+    [SerializeField] private Vector3 maxRotation;
+    private IDisposable disposable;
+
+    private void Awake()
+    {
+        InputSystem.onEvent += LimitRotation;
+    }
+
+    private void OnDisable()
+    {
+        InputSystem.onEvent -= LimitRotation;
+    }
+
+    void LimitRotation(InputEventPtr input, InputDevice device)
+    {
+        Debug.Log("---");
+        Debug.Log(transform.eulerAngles);
+        Vector3 currentAngle = transform.rotation.eulerAngles;
+        if (currentAngle.x > 180)
+        {
+            currentAngle.x -= 360f;
+        }
+        if (currentAngle.y > 180)
+        {
+            currentAngle.y -= 360f;
+        }
+        if (currentAngle.z > 180)
+        {
+            currentAngle.z -= 360f;
+        }
+        Vector3 clamped_angle = new Vector3(
+            Mathf.Clamp(currentAngle.x, minRotation.y, maxRotation.x),
+            Mathf.Clamp(currentAngle.y, minRotation.y, maxRotation.y),
+            Mathf.Clamp(currentAngle.z, minRotation.z, maxRotation.z)
+        );
+        Debug.Log(clamped_angle);
+
+        transform.eulerAngles = clamped_angle;
+    }
+}
