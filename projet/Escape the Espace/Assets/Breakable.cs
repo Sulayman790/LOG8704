6826d2ba69544ac05585020cd631c7f85feab212
Xavier Chalifoux <xav.chalif@hotmail.com>
2024-11-25 22:34:08 -0500

    Orion game working

---
+++ projet/Escape the Espace/Assets/Breakable.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Breakable : MonoBehaviour
+{
+    public List<GameObject> breakablePieces;
+    public static event System.Action OnBreakableDestroyed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        foreach (var item in breakablePieces){
+            item.SetActive(false);
+        }
+    }
+
+    public void Break(){
+        foreach (var item in breakablePieces)
+        {
+            item.SetActive(true);
+            item.transform.parent = null;
+        }
+        gameObject.SetActive(false);
+        OnBreakableDestroyed?.Invoke();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Sword"))
+        {
+            Break();
+        }
+    }
+}
