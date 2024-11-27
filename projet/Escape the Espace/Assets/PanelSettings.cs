3e146c73dfa40f97a0cae1fceb469f56d24c32ac
Mahdi <gmx_mahdi@outlook.com>
2024-11-26 19:53:51 -0500

    Add audio control (global + mute yapper)

--- projet/Escape the Espace/Assets/PanelSettings.cs
+++ projet/Escape the Espace/Assets/PanelSettings.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class PanelSettings : PanelScreen
 {
+    private AudioSource yappingDude;
     void Start()
     {
-        
+        yappingDude = GameObject.Find("Story").GetComponent<AudioSource>();
     }
 
     public override void UpdateScreen()
@@ -13,11 +14,17 @@ public class PanelSettings : PanelScreen
 
     public void SetVolume(float value)
     {
-
+        AudioListener.volume = value;
     }
 
     public void SetGraphics(int quality)
     {
         QualitySettings.SetQualityLevel(quality);
+    }
+
+    public void ToggleVoiceMute(bool mute)
+    {
+        if (mute) yappingDude.volume = 0;
+        else yappingDude.volume = 1;
     }
 }
